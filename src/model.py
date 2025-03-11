c10c36e5decb8e0cd585f75cb7b8e65d62907a3d
toan.nguyen19 <toan.nguyen19@mservice.com.vn>
2025-03-11 12:15:12 +0700

    [update] refactor model code

--- src/model.py
+++ src/model.py
@@ -2,67 +2,41 @@ import torch
 from torch import nn
 from torch.nn import functional as F
 
-################################################################
 class Expert(nn.Module):
     def __init__(self, config):
         super().__init__()
         n_embed = config.n_embed
         dropout = config.expert_dropout
         self.net = nn.Sequential(
-            nn.Linear(n_embed, 4* n_embed),
+            nn.Linear(n_embed, 4 * n_embed),
             nn.ReLU(),
             nn.Linear(4 * n_embed, n_embed),
-            nn.Dropout(dropout))
+            nn.Dropout(dropout)
+        )
 
     def forward(self, x):
         return self.net(x)
 
-################################################################
 class MoeLayer(nn.Module):
-    """
-    A Mixture of Experts (MoE) layer.
-
-    Args:
-        experts (list of nn.Module): A list of expert networks.
-        gate (nn.Module): A gating network that outputs logits for selecting experts.
-        k (int, optional): The number of experts to select for each input. Default is 1.
-
-    Methods:
-        forward(inputs: torch.Tensor) -> torch.Tensor:
-            Forward pass through the MoE layer. Selects `k` experts based on the gate logits
-            and combines their outputs weighted by the gate probabilities.
-
-    Attributes:
-        experts (nn.ModuleList): A list of expert networks.
-        gate (nn.Module): A gating network that outputs logits for selecting experts.
-        k (int): The number of experts to select for each input.
-    """
-    def __init__(self, experts, gate, k: int =1):
+    def __init__(self, experts, gate, k=1):
         super().__init__()
         assert len(experts) > 0
         self.experts = nn.ModuleList(experts)
         self.gate = gate
         self.k = k
 
-    def forward(self, inputs: torch.Tensor):
+    def forward(self, inputs):
         inputs_squashed = inputs.view(-1, inputs.shape[-1])
         gate_logits = self.gate(inputs_squashed)
-        weights, selected_experts = torch.topk(
-            gate_logits, self.k
-        )
-        weights = nn.functional.softmax(
-            weights,
-            dim=1,
-            dtype=torch.float,
-        ).type_as(inputs)
+        topk_values, selected_experts = torch.topk(gate_logits, self.k)
+        weights = F.softmax(topk_values, dim=1).type_as(inputs)
         results = torch.zeros_like(inputs_squashed)
+        
         for i, expert in enumerate(self.experts):
             batch_idx, nth_expert = torch.where(selected_experts == i)
-            results[batch_idx] += weights[batch_idx, nth_expert, None] * expert(
-                inputs_squashed[batch_idx]
-            )
+            results[batch_idx] += weights[batch_idx, nth_expert] * expert(inputs_squashed[batch_idx])
+        
         return results.view_as(inputs)
-
 
 class Head(nn.Module):
     def __init__(self, config, head_size):
@@ -87,40 +61,36 @@ class Head(nn.Module):
         out = wei @ v
         return out
 
-
 class MultiHeadAttention(nn.Module):
     def __init__(self, config):
         super().__init__()
         n_head = config.n_head
-        head_size = config.n_embed//n_head
-        self.heads = [Head(config, head_size) for _ in range(n_head)]
+        head_size = config.n_embed // n_head
+        self.heads = nn.ModuleList([Head(config, head_size) for _ in range(n_head)])
         self.dropout = nn.Dropout(config.attention_dropout)
+
     def forward(self, x):
         out = torch.cat([head(x) for head in self.heads], dim=-1)
         out = self.dropout(out)
-
         return out
 
 class BlockLayer(nn.Module):
     def __init__(self, config, idx):
         super().__init__()
-        n_head = config.n_head
         n_embed = config.n_embed
         n_experts = config.n_experts
         self.sa_head = MultiHeadAttention(config)
         self.smoe = MoeLayer(
             experts=[Expert(config) for _ in range(n_experts)],
-            gate=nn.Linear(n_embed, n_experts, bias=False),
+            gate=nn.Linear(n_embed, n_experts, bias=False)
         )
-
         self.ln1 = nn.LayerNorm(n_embed)
         self.ln2 = nn.LayerNorm(n_embed)
 
     def forward(self, x):
-        out = x + self.ln1(self.sa_head(x))
-        out = out + self.ln2(self.smoe(x))
-        return out 
-    
+        moe_output = x + self.ln1(self.sa_head(x))
+        moe_output = moe_output + self.ln2(self.smoe(moe_output))
+        return moe_output
 
 class LLM(nn.Module):
     def __init__(self, config):
@@ -131,15 +101,17 @@ class LLM(nn.Module):
         n_layer = config.n_layer
         block_size = config.block_size
         self.embedding = nn.Embedding(vocab_size, n_embed)
-        self.position_embedding_table = nn.Embedding(block_size, n_embed)    
-        self.encoders = [BlockLayer(config, layer_i) for layer_i in range(n_layer)]
-        self.lm_head =  nn.Linear(n_embed, vocab_size)
+        self.position_embedding_table = nn.Embedding(block_size, n_embed)
+        self.encoders = nn.ModuleList([BlockLayer(config, layer_i) for layer_i in range(n_layer)])
+        self.register_buffer('position_ids', torch.arange(block_size).to(self.device))
+        self.lm_head = nn.Linear(n_embed, vocab_size)
 
-    def forward(self, idx, labels = None, **kwargs):
+    def forward(self, idx, labels=None):
         B, T = idx.shape
         token_embedding_vec = self.embedding(idx)
-        pos_embedding_vec = self.position_embedding_table(torch.arange(T)   .to(self.device))
+        pos_embedding_vec = self.position_embedding_table(self.position_ids[:T])
         x = token_embedding_vec + pos_embedding_vec
+        
         for encoder in self.encoders:
             x = encoder(x)
 
@@ -147,12 +119,9 @@ class LLM(nn.Module):
         
         if labels is not None:
             B, T, C = logits.shape
-            logits = logits.view(B*T, C)
-            targets = labels.view(B*T)
+            logits = logits.view(B * T, C)
+            targets = labels.view(B * T)
             loss = F.cross_entropy(logits, targets)
             return loss
         else:
             return logits
-
-    
- 
